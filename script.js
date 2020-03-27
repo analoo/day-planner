b830bb54b6ed9f855dbdacf2ddd8284435025a8d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-26 22:46:31 -0700

    figured out how to update my planner data with new data when user saves

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 var plannerData = [
-   {time: 9, display: "9AM", activity: "String of text for nine"},
-   {time: 10, display: "10AM", activity: "String of text for ten"},
-   {time: 11, display: "11AM", activity: "String of text for eleven"},
-   {time: 12, display: "12PM", activity: "String of text for twelve"},
-   {time: 13, display: "1PM", activity: "String of text for one"},
-   {time: 14, display: "2PM", activity: "String of text for two"},
-   {time: 15, display: "3PM", activity: "String of text for three"},
-   {time: 16, display: "4PM", activity: "String of text for four"},
-   {time: 17, display: "5PM", activity: "String of text for five"},
+    { time: 9, display: "9AM", activity: "String of text for nine" },
+    { time: 10, display: "10AM", activity: "String of text for ten" },
+    { time: 11, display: "11AM", activity: "String of text for eleven" },
+    { time: 12, display: "12PM", activity: "String of text for twelve" },
+    { time: 13, display: "1PM", activity: "String of text for one" },
+    { time: 14, display: "2PM", activity: "String of text for two" },
+    { time: 15, display: "3PM", activity: "String of text for three" },
+    { time: 16, display: "4PM", activity: "String of text for four" },
+    { time: 17, display: "5PM", activity: "String of text for five" },
 ]
 
 // Function that updates the time every second
@@ -21,44 +21,56 @@ function updateTime() {
 }
 
 updateTime();
+renderPlannerData();
+
 
 // loop for showing all data in browser
 
 $("#currentDay").text(currentDate);
 
-for (let i = 0; i< plannerData.length ; i++){
-    var trHTML = $("<tr></tr>");
-    var imageSrc = "Assets/save.png"
-    trHTML.addClass("row");
-    $(".container").append(trHTML);
-    var timeDisplay = $("<td>"+plannerData[i].display +"</td>")
-    var activityDisplay = $("<td><textarea>"+plannerData[i].activity +"</textarea></td>")
-    var saveDisplay = $("<td><img src="+imageSrc+"></td>")
-    timeDisplay.addClass("hour");
-    activityDisplay.addClass("description");
-    saveDisplay.addClass("saveBtn");
-    saveDisplay.attr("id", "li"+i)
+function renderPlannerData() {
+    for (let i = 0; i < plannerData.length; i++) {
+        var trHTML = $("<tr></tr>");
+        var imageSrc = "Assets/save.png"
+        trHTML.addClass("row");
+        $(".container").append(trHTML);
 
-    if(plannerData[i].time == currentTime){
-        activityDisplay.addClass("present");
+        var timeDisplay = $("<td>" + plannerData[i].display + "</td>")
+        timeDisplay.addClass("hour");
+
+        var activityDisplay = $("<td><textarea id='li" + i + "'>" + plannerData[i].activity + "</textarea></td>")
+        activityDisplay.addClass("description");
+        // activityDisplay.attr("id", "li"+i)
+
+        var saveDisplay = $("<td><img src=" + imageSrc + "></td>")
+        saveDisplay.addClass("saveBtn");
+        saveDisplay.data("id", "li" + i)
+
+
+        if (plannerData[i].time == currentTime) {
+            activityDisplay.addClass("present");
+        }
+
+        else if (plannerData[i].time > currentTime) {
+            activityDisplay.addClass("future");
+        }
+
+        else if (plannerData[i].time < currentTime) {
+            activityDisplay.addClass("past");
+        }
+
+        $(trHTML).append(timeDisplay, activityDisplay, saveDisplay);
+
     }
+}
+;
 
-    else if(plannerData[i].time > currentTime){
-        activityDisplay.addClass("future");
-    }
+$(".saveBtn").on("click", function () {
+    var index = $(this).data("id");
+    var indNoLi = index.split("li")[1];
+    var newDescValue = $("#" + index).val();
+    plannerData[indNoLi].activity = newDescValue;
+    renderPlannerData();
 
-    else if(plannerData[i].time < currentTime){
-        activityDisplay.addClass("past");
-    }
 
-    $(trHTML).append(timeDisplay, activityDisplay, saveDisplay);
-
-};
-
-$(".saveBtn").on("click", function(){
-    let rowIndex = this.id.split("li")[1];
-    console.log($(this).parent())
-
-    
 })
-
