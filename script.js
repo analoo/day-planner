31f1790eeaf0d686827e57a970da4202df2c3dd7
Ana Medrano <amedrano.fernandez@gmail.com>
2020-03-27 00:24:21 -0700

    thoroughly commented code and changed some variable names for readability

--- script.js
+++ script.js
@@ -5,19 +5,24 @@ var currentDate;
 
 startPlanner();
 
-function startPlanner(){
+// function to initiate the planner process
+
+function startPlanner() {
     updateTime();
+
+    $("#currentDay").text(currentDate);
+    // time and display values are preset based on class requirements
     if (JSON.parse(localStorage.getItem("pd-AMF")) == null) {
         plannerData = [
-        { time: 9, display: "9AM", activity: "" },
-        { time: 10, display: "10AM", activity: "" },
-        { time: 11, display: "11AM", activity: "" },
-        { time: 12, display: "12PM", activity: "" },
-        { time: 13, display: "1PM", activity: "" },
-        { time: 14, display: "2PM", activity: "" },
-        { time: 15, display: "3PM", activity: "" },
-        { time: 16, display: "4PM", activity: "" },
-        { time: 17, display: "5PM", activity: "" },
+            { time: 9, display: "9AM", activity: "" },
+            { time: 10, display: "10AM", activity: "" },
+            { time: 11, display: "11AM", activity: "" },
+            { time: 12, display: "12PM", activity: "" },
+            { time: 13, display: "1PM", activity: "" },
+            { time: 14, display: "2PM", activity: "" },
+            { time: 15, display: "3PM", activity: "" },
+            { time: 16, display: "4PM", activity: "" },
+            { time: 17, display: "5PM", activity: "" },
         ]
         localStorage.setItem("pd-AMF", JSON.stringify(plannerData));
     }
@@ -25,42 +30,45 @@ function startPlanner(){
     else {
         plannerData = JSON.parse(localStorage.getItem("pd-AMF"))
     }
+    // page elements are dynamically created based on objects in planner data
     renderPlannerData();
-    setInterval(updateTime(),1000)
+    // time is updated every 30 seconds to ensure highlighting matches current time
+    setInterval(updateTime(), 30 * 1000)
 
 }
-
-// Function that updates the time every second
-
-
+// function that formats moment to extract time and date
 function updateTime() {
     currentTime = moment().format("H");
     currentDate = moment().format("dddd, MMMM Do");
 }
 
-// loop for showing all data in browser
-
-$("#currentDay").text(currentDate);
-
 function renderPlannerData() {
+    // takes planner data and dynamically creates HTML elements
     for (let i = 0; i < plannerData.length; i++) {
+
+        // first a table row is created to how the 3 elements for each hour
         var trHTML = $("<tr></tr>");
-        var imageSrc = "Assets/save.png"
         trHTML.addClass("row");
         $(".container").append(trHTML);
 
+        // pulls in display value from planner data
         var timeDisplay = $("<td>" + plannerData[i].display + "</td>")
         timeDisplay.addClass("hour");
 
+        // creates text area and table data elements and assigns a unique id
         var activityDisplay = $("<td><textarea id='li" + i + "'>" + plannerData[i].activity + "</textarea></td>")
         activityDisplay.addClass("description");
-        // activityDisplay.attr("id", "li"+i)
-
+        
+        // appends image to save display, adds a data elemtn called id that matches the id in the table display
+        var imageSrc = "Assets/save.png"
         var saveDisplay = $("<td><img src=" + imageSrc + "></td>")
         saveDisplay.addClass("saveBtn");
-        saveDisplay.data("id", "li" + i)
+        saveDisplay.data("id", "li" + i);
 
+        // assigns click listener to all save display buttons
+        saveDisplay.on("click", saveActivity);
 
+        // for each element, checks value relative to time and adds appropriate class for formatting
         if (plannerData[i].time == currentTime) {
             activityDisplay.addClass("present");
         }
@@ -75,17 +83,28 @@ function renderPlannerData() {
 
         $(trHTML).append(timeDisplay, activityDisplay, saveDisplay);
 
+
     }
 }
 ;
 
-$(".saveBtn").on("click", function () {
-    var index = $(this).data("id");
-    var indNoLi = index.split("li")[1];
-    var newDescValue = $("#" + index).val();
-    plannerData[indNoLi].activity = newDescValue;
+// $(document.body).on('click', '.saveBtn' , clickEvent);
+
+// click event that triggers saving data to local storage
+function saveActivity() {
+    // pulls data id value from save button
+    var dataID = $(this).data("id");
+    // turns value into integer value, matching to desired index
+    var index = dataID.split("li")[1];
+    // extracts the value of the activity description
+    var newDescValue = $("#" + dataID).val();
+    // and assigns it back to the planner data
+    plannerData[index].activity = newDescValue;
+    // this is then stored locally
     localStorage.setItem("pd-AMF", JSON.stringify(plannerData));
+    // the rendered HTML is cleared
+    $(".container").empty();
+    // and the renderPlannerData function is reran
     renderPlannerData();
 
-})
-
+}
